f986c4515d1da9798b665a7fbeb4a614271986ee
Steve <steve.sharmokh@gmail.com>
2019-08-02 04:22:08 -0700

    Initialize Project through Spring Boot.  Closes #1

---
+++ src/main/java/com/tazks/app/TazksApplication.java
@@ -0,0 +1,13 @@
+package com.tazks.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TazksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TazksApplication.class, args);
+    }
+
+}
